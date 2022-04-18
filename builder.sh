e589866665cd32025c17b6e1985316a85eb4ce24
SkYe231 <skye231@foxmail.com>
2022-04-18 22:41:14 +0800

    v0.0.5

--- builder.sh
+++ builder.sh
@@ -7,27 +7,30 @@ cd buildroot
 make clean
 cp ../config/${TARGETARCH}-config .config
 make
-export PATH=~/buildroot/output/host/bin:$PATH
+export PATH=`pwd`/output/host/bin:$PATH
 cd ..
 
-cd gdb-${GDB_VERSION}
-mkdir build
-mkdir out
-../configure\
- --prefix=../out\
- --program-suffix=-${TARGETARCH}-${GDB_VERSION}\
- --host=${TARGETARCH}-linux\
- --disable-werror\
- --enable-debug\
- --disable-shared\
- --enable-static
-make -j8 && make install
-cd ../out/bin
-tar tar -zcvf ${TARGETARCH}-${GDB_VERSION}.tar.gz *
-cp ${TARGETARCH}-${GDB_VERSION}.tar.gz /releases/
-cd ../..
-rm -rf build out
-cd ..
+ls buildroot/output/host/bin
+${TARGETARCH}-linux-gcc --version
+
+# cd gdb-${GDB_VERSION}
+# mkdir build
+# mkdir out
+# ../configure\
+#  --prefix=../out\
+#  --program-suffix=-${TARGETARCH}-${GDB_VERSION}\
+#  --host=${TARGETARCH}-linux\
+#  --disable-werror\
+#  --enable-debug\
+#  --disable-shared\
+#  --enable-static
+# make -j8 && make install
+# cd ../out/bin
+# tar tar -zcvf ${TARGETARCH}-${GDB_VERSION}.tar.gz *
+# cp ${TARGETARCH}-${GDB_VERSION}.tar.gz /releases/
+# cd ../..
+# rm -rf build out
+# cd ..
 
 
 ls /releases
