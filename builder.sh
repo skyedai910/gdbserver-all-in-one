628e43bac50059473aa44c9a71d077d7ff27715b
SkYe231 <skye231@foxmail.com>
2022-04-19 00:16:31 +0800

    v0.0.6

--- builder.sh
+++ builder.sh
@@ -8,29 +8,29 @@ make clean
 cp ../config/${TARGETARCH}-config .config
 make
 export PATH=`pwd`/output/host/bin:$PATH
+tar -zcvf ${TARGETARCH}-buildroot.tar.gz output/host/bin/
+cp ${TARGETARCH}-buildroot.tar.gz /releases/
 cd ..
 
-ls buildroot/output/host/bin
-${TARGETARCH}-linux-gcc --version
-
-# cd gdb-${GDB_VERSION}
-# mkdir build
-# mkdir out
-# ../configure\
-#  --prefix=../out\
-#  --program-suffix=-${TARGETARCH}-${GDB_VERSION}\
-#  --host=${TARGETARCH}-linux\
-#  --disable-werror\
-#  --enable-debug\
-#  --disable-shared\
-#  --enable-static
-# make -j8 && make install
-# cd ../out/bin
-# tar tar -zcvf ${TARGETARCH}-${GDB_VERSION}.tar.gz *
-# cp ${TARGETARCH}-${GDB_VERSION}.tar.gz /releases/
-# cd ../..
-# rm -rf build out
-# cd ..
+cd gdb-${GDB_VERSION}
+mkdir build
+mkdir out
+cd build
+../configure\
+ --prefix=../out\
+ --program-suffix=-${TARGETARCH}-${GDB_VERSION}\
+ --host=${TARGETARCH}-linux\
+ --disable-werror\
+ --enable-debug\
+ --disable-shared\
+ --enable-static
+make -j8 && make install
+cd ../out/bin
+tar -zcvf ${TARGETARCH}-${GDB_VERSION}-gdb.tar.gz *
+cp ${TARGETARCH}-${GDB_VERSION}-gdb.tar.gz /releases/
+cd ../..
+rm -rf build out
+cd ..
 
 
 ls /releases
