d14a169ddc7b040b6f8568e5dc441bc0de01d283
SkYe231 <skye231@foxmail.com>
2022-04-18 17:14:34 +0800

    v0.0.2

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,7 @@
-sudo apt-get update
-sudo apt-get install libncurs*
-sudo apt-get install libncurses5-dev patch
+sudo apt-get update -y
+sudo apt-get install libncurs* -y
+sudo apt-get install libncurses5-dev patch -y
+
+git clone git@github.com:buildroot/buildroot.git
+cd buildroot
+make clean
