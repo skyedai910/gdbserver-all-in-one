b0af40ed742bc1ee70338fe6ff67184b7830b3d2
SkYe231 <skye231@foxmail.com>
2022-04-18 20:11:56 +0800

    v0.0.4

--- setup_env.sh
+++ setup_env.sh
@@ -6,6 +6,6 @@ sudo apt-get install libncurses5-dev patch -y
 
 mkdir /releases
 
-git clone git@github.com:buildroot/buildroot.git
+git clone https://github.com/buildroot/buildroot.git
 wget https://ftp.gnu.org/gnu/gdb/gdb-${GDB_VERSION}.tar.gz
 tar xf gdb-${GDB_VERSION}.tar.gz
