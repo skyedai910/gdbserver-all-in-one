3d41ea3b1a60d4210901e7609a6f78d708599948
SkYe231 <skye231@foxmail.com>
2022-04-18 19:34:08 +0800

    v0.0.3

---
+++ builder.sh
@@ -0,0 +1,28 @@
+cd buildroot
+make clean
+cp ../config/${TARGETARCH}-config .config
+make
+export PATH=~/buildroot/output/host/bin:$PATH
+cd ..
+
+cd gdb-${GDB_VERSION}
+mkdir build
+mkdir out
+../configure\
+ --prefix=../out\
+ --program-suffix=-${TARGETARCH}-${GDB_VERSION}\
+ --host=mips-linux\
+ --disable-werror\
+ --enable-debug\
+ --disable-shared\
+ --enable-static
+make -j8 && make install
+cd ../out/bin
+tar tar -zcvf ${TARGETARCH}-${GDB_VERSION}.tar.gz *
+cp ${TARGETARCH}-${GDB_VERSION}.tar.gz /releases/
+cd ../..
+rm -rf build out
+cd ..
+
+
+ls /releases
--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
 sudo apt-get update -y
 sudo apt-get install libncurs* -y
 sudo apt-get install libncurses5-dev patch -y
 
+mkdir /releases
+
 git clone git@github.com:buildroot/buildroot.git
-cd buildroot
-make clean
+wget https://ftp.gnu.org/gnu/gdb/gdb-${GDB_VERSION}.tar.gz
+tar xf gdb-${GDB_VERSION}.tar.gz
