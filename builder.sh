638b42854f5c76bc7f94104282996ec9ae93e080
SkYe231 <skye231@foxmail.com>
2022-04-19 22:52:25 +0800

    v0.0.16

--- builder.sh
+++ builder.sh
@@ -3,6 +3,7 @@
 GDB_VERSION=$1
 TARGETARCH=$2
 
+
 echo "Start build buildroot"
 cd buildroot
 make clean
@@ -11,36 +12,35 @@ make
 export PATH=`pwd`/output/host/bin:$PATH
 tar -zcvf ${TARGETARCH}-buildroot.tar.gz output/host/*
 cp ${TARGETARCH}-buildroot.tar.gz /releases/
+
+echo "test buildroot"
+${TARGETARCH}-linux-gcc --version
+
 cd ..
+
 
 echo "Start build gdb"
 HOMEDIR=`pwd`
 wget https://ftp.gnu.org/gnu/gdb/gdb-${GDB_VERSION}.tar.gz
 tar xf gdb-${GDB_VERSION}.tar.gz
-cd gdb-${GDB_VERSION}/gdbserver
+cd gdb-${GDB_VERSION}
 mkdir build
 mkdir out
 cd build
 ../configure\
- --prefix=`pwd`/../out\
- --program-prefix=${TARGETARCH}-linux-${GDB_VERSION}-\
+ --prefix=${HOMEDIR}/gdb-${GDB_VERSION}/out\
+ --program-prefix=${TARGETARCH}-linux-${GDB_VERSION}\
  --host=${TARGETARCH}-linux\
  --disable-werror\
- --enable-debug\
- --disable-shared\
- --enable-static\
- CC=${HOMEDIR}/buildroot/output/host/bin/${TARGETARCH}-linux-gcc\
- CXX=${HOMEDIR}/buildroot/output/host/bin/${TARGETARCH}-linux-g++
-make -j16
+ LDFLAGS=-static
+make -j 16
 make install
 cd ../out/bin
 tar -zcvf ${TARGETARCH}-linux-${GDB_VERSION}-gdb.tar.gz *
 cp ${TARGETARCH}-linux-${GDB_VERSION}-gdbserver.tar.gz /releases/
 cd ${HOMEDIR}
-
-pwd && ls
-
 rm -rf gdb-${GDB_VERSION} gdb-${GDB_VERSION}.tar.gz
+
 
 echo "Check releases"
 ls /releases
